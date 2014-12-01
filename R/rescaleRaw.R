5c79e25be11662c8f876f2ba855495ebbe8f2ff9
David Eccles (gringer) <github@gringer.org>
2014-12-02 12:56:05 +1300

    bugfix -- variable name changed from function

--- R/rescaleRaw.R
+++ R/rescaleRaw.R
@@ -1,10 +1,10 @@
 rescaleRaw <- function(rawSignal, channelMeta, runMedian=FALSE){
     ## Converts from raw signal to pA, using channel metadata
     if(runMedian){
-        (runmed(x,7)+channelMeta["offset"]) * 
+        (runmed(rawSignal,7)+channelMeta["offset"]) * 
            channelMeta["range"]/channelMeta["digitisation"];
     } else {
-        (x+channelMeta["offset"]) * 
+        (rawSignal+channelMeta["offset"]) * 
            channelMeta["range"]/channelMeta["digitisation"];
     }
 }
